8a39768667344ddeb8d13da1b4ac2477d363ea8d
li_1987245 <li_1987245@163.com>
2017-09-01 18:36:12 +0800

    init

--- spark/structured_streaming.py
+++ spark/structured_streaming.py
@@ -4,16 +4,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, window
 from pyspark.sql.functions import split
 
-'''
-The “Output” is defined as what gets written out to the external storage. The output can be defined in a different mode:
-
-Complete Mode - The entire updated Result Table will be written to the external storage. It is up to the storage connector to decide how to handle writing of the entire table.
-
-Append Mode - Only the new rows appended in the Result Table since the last trigger will be written to the external storage. This is applicable only on the queries where existing rows in the Result Table are not expected to change.
-
-Update Mode - Only the rows that were updated in the Result Table since the last trigger will be written to the external storage (available since Spark 2.1.1). Note that this is different from the Complete Mode in that this mode only outputs the rows that have changed since the last trigger. If the query doesn’t contain aggregations, it will be equivalent to Append mode.
-'''
-
 spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
 # Create DataFrame representing the stream of input lines from connection to localhost:9999
 lines = spark \
@@ -63,6 +53,15 @@ windowedCounts = words.groupBy(
 # Generate running word count
 # wordCounts = words.groupBy("word").count()
 # Start running the query that prints the running counts to the console,output model complete/append/update(2.1.1+)
+'''
+The “Output” is defined as what gets written out to the external storage. The output can be defined in a different mode:
+
+Complete Mode - The entire updated Result Table will be written to the external storage. It is up to the storage connector to decide how to handle writing of the entire table.
+
+Append Mode - Only the new rows appended in the Result Table since the last trigger will be written to the external storage. This is applicable only on the queries where existing rows in the Result Table are not expected to change.
+
+Update Mode - Only the rows that were updated in the Result Table since the last trigger will be written to the external storage (available since Spark 2.1.1). Note that this is different from the Complete Mode in that this mode only outputs the rows that have changed since the last trigger. If the query doesn’t contain aggregations, it will be equivalent to Append mode.
+'''
 query = windowedCounts\
         .writeStream\
         .outputMode('complete')\
