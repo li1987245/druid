2851b64be6909b677f4514248248a58fa9884396
li_1987245 <li_1987245@163.com>
2018-02-04 16:54:59 +0800

    merge

--- scikitlearn/predict/house/price_predict.py
+++ scikitlearn/predict/house/price_predict.py
@@ -5,7 +5,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.pipeline import Pipeline
 from xgboost.sklearn import XGBRegressor
+import matplotlib.pyplot as plt
 
-
+# http://blog.csdn.net/youyuyixiu/article/details/72841703
 def load_data():
-    train = pd.read_csv(r'F:\data\tmdb\tmdb_5000_movies.csv')
+    train = pd.read_csv(r'F:\data\house\train.csv')
+    # print(train.info())
+    # print(train.sample())
